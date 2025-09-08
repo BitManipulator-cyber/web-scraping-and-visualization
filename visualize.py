6e969b968b632cb8971f4f50aa6ba4563e0369eb
Aaditya Jagtap <aadityajagtap2205@gmail.com>
2025-09-08 22:29:19 +0530

    Assigned Anjali's and Namya's Task

---
+++ visualize.py
@@ -0,0 +1,22 @@
+"""
+Anjali & Namya's Part
+
+Goal : Basically we have to predict that how many passengers will come back and how much will be the revenue for 
+       the next year i.e, 2026.
+
+Context: In this same project open Samples/Flight.csv it is basically a dataset of Flight's revenue over the years
+         ranging from 1990 to 2025
+
+         So, Atharva will be predicting the revenue and passenger return rate for year 2025 based on data of 1990-2025.
+         If you have any doubt ask me or Atharva of you are reading.
+
+Task:
+    1. Take the predicted outputs
+    2. Visualize the predicted 
+
+Possible Options to create:
+        1. Check the dashboard.py in this same project folder and run it using ``python dashboard.py``
+           Basically it create a server which runs on the local system and we can see beatiful and interactive dashboards there
+        2. Add you options here...
+
+"""
