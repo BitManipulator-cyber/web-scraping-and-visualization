f95b0ec05d01262ff72731b77e64345a61d2ff5e
Aaditya Jagtap <aadityajagtap2205@gmail.com>
2025-09-08 22:30:27 +0530

    Assigned Atharva's task

--- training.py
+++ training.py
@@ -1,5 +1,13 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+"""
+Atharva's Part
 
-df = pd.read_csv('Samples/Companies.csv')
+Task:
+    1. Load the Dataset
+    2. Preprocessing
+    3. Split the dataset
+    4. Evaluate which feature to keep and which to remove
+    5. Select the best feature
+    6. Use that feature and train LSTM
+    7. Visualize the basic part
+
+"""
